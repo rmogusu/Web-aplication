71fed2ff0fda0bc1211c91f2782b954f2d3f8ad7
mrsfreddy <rmogusu123@gmal.com>
2019-07-15 03:51:59 -0400

    adit my form

--- script.js
+++ script.js
@@ -3,34 +3,34 @@ var maleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
 var femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
 
 
-function calculateBirthday() {
+function calculate() {
  var valid=true;
- var form = document.forms["myForm"];
- var dd = form["date"].value;
- var mm = form["month"].value;
- var cc = form["century"].value;
- var yy = form["year"].value;
+ var form = document.forms["mainForm"];
+ var dd = form["century"].value;
+ var mm = form["year"].value;
+ var cc = form["month"].value;
+ var yy = form["day"].value;
  var gender=form["gender"].value;
 
  if (dd <= 0 || dd > 31){
-   alert ("Invalid date. Enter valid date.");
+   alert ("Invalid . Enter valid one.");
    valid=false;
  }
  if (mm <= 0 || mm > 12){
-   alert("Invalid month. Enter valid month");
+   alert("Invalid . Enter valid one");
    valid=false;
  }
 
  if (valid) {
-   var result = dayOfTheWeek(dd,mm,cc,yy);
+   var result = weekDay(dd,mm,cc,yy);
    result = result.toFixed(0);
    if (gender==='male') {
      alert ("Your Akan name is "+maleNames[result]);
-     document.getElementById("akan").innerHTML="Akan name is"+maleNames[result]
+     document.getElementById("akan").innerHTML="your Akan name is"+maleNames[result]
    }
    else{
      alert("You Akan name is "+femaleNames[result]);
-     document.getElementById("akan").innerHTML="Akan name is"+femaleNames[result]
+     document.getElementById("akan").innerHTML="Your Akan name is"+femaleNames[result]
    }
  }
 }
