e741f3b31fc0bfb9edd220394860d8d1c8b9deb7
mrsfreddy <rmogusu123@gmal.com>
2019-07-15 01:21:13 -0400

    editmy form

--- script.js
+++ script.js
@@ -1,22 +1,11 @@
-<<<<<<< HEAD
-var maleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
-var femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama",]
 
-
-function dayOfTheWeek(dd,mm,cc,yy) {
- return (((cc/4)-2*cc-1)+(5*yy/4)+(26*(mm+1)/10)+dd)%7;
-}
-=======
-var calculate = function(dd,mm,cc,yy){
-  return(((cc/4)-2*cc-1)+(5*yy/4)+(26*(mm+1)/10)+dd)%7;
- }
 var maleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
 var femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
->>>>>>> master
 
- var result = function () {
+
+function calculateBirthday() {
  var valid=true;
- var form = document.form["myForm"]
+ var form = document.forms["myForm"];
  var dd = form["date"].value;
  var mm = form["month"].value;
  var cc = form["century"].value;
@@ -27,14 +16,15 @@ var femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
    alert ("Invalid date. Enter valid date.");
    valid=false;
  }
-  else  if (mm <= 0 || mm > 12){
+ if (mm <= 0 || mm > 12){
    alert("Invalid month. Enter valid month");
    valid=false;
  }
- else if (valid) {
-   var result = calculate(dd,mm,cc,yy);
+
+ if (valid) {
+   var result = dayOfTheWeek(dd,mm,cc,yy);
    result = result.toFixed(0);
-   if (gender==="male") {
+   if (gender==='male') {
      alert ("Your Akan name is "+maleNames[result]);
      document.getElementById("akan").innerHTML="Akan name is"+maleNames[result]
    }
